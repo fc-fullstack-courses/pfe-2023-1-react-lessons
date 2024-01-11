62857cad9775545e53422c4a01eab8629dd00d57
Kyrylo <fresh.js.ftd.@gmail.com>
2024-01-11 18:18:38 +0200

    Add ClassComponent example

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+/*
+  Компонентів існує 2 види
+    класові
+    функціональні
+*/
+
+// класовий компонент
+// має успадковуватися від React.Component 
+class Heading extends React.Component {
+
+  // має мати метод render
+  render () {
+    // має повертати реакт елемент / компонент
+    return <h1>Some heading</h1>;
+  }
+}
+
+// відмальовування компоненту
+// const headerComponent = React.createElement(Heading);
+const headerComponent = <Heading />;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    
-  </React.StrictMode>
-);
+root.render(headerComponent);
