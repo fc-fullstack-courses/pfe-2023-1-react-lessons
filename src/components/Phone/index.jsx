1a047f6e5b98308b9d26fe6369eec6fdb44fe592
Kyrylo <fresh.js.ftd.@gmail.com>
2024-01-12 18:57:57 +0200

    Add multiple components conditional rendering

--- src/components/Phone/index.jsx
+++ src/components/Phone/index.jsx
@@ -13,12 +13,12 @@ class Phone extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick () {
+  handleClick() {
     // setState - метод який приймає об'єкт і зливає його з поточним станом
     // після чого реакт робить повторний рендер
     const stateChanges = {
-      phoneState: 'on'
-    }
+      phoneState: 'on',
+    };
     this.setState(stateChanges);
     /*
       доробити телефону кнопку вимикання, 
@@ -29,24 +29,47 @@ class Phone extends React.Component {
   // this береться автоматично
   handleToggleOff = () => {
     this.setState({
-      phoneState: 'off'
+      phoneState: 'off',
     });
-  }
+  };
 
   render() {
     const { color, price } = this.props;
     const { phoneState } = this.state;
 
+    // let phoneStatus;
+
+    // if(phoneState === 'on') {
+    //   phoneStatus = <PhoneOnText />;
+    // } else {
+    //   phoneStatus = <PhoneOffText/>;
+    // }
+
+    const phoneStatus =
+      phoneState === 'on' ? <PhoneOnText /> : <PhoneOffText />;
+
     return (
-      <div className='phoneContainer'>
+      <div className="phoneContainer">
         <p>Phone is {color}</p>
         <p>Phone price is {price}</p>
-        <p>Phone is currently {phoneState}</p>
+        {phoneStatus}
         <button onClick={this.handleClick}>Switch on</button>
         <button onClick={this.handleToggleOff}>Switch off</button>
       </div>
     );
   }
+}
+
+/*
+  умовний рендерінг полягає у відмальуванні певних компонентів / елементів / тексту
+  за певної умови
+*/
+function PhoneOnText() {
+  return <p>Phone is currently on</p>;
+}
+
+function PhoneOffText() {
+  return <p>Phone is currently off</p>;
 }
 
 export default Phone;
