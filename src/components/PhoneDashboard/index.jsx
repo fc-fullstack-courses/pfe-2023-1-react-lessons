89e8b3e28cf63014ec388fe487aaa1fb3049b6ba
Kyrylo <fresh.js.ftd.@gmail.com>
2024-01-15 20:46:49 +0200

    Refactor phone related components structure

--- src/components/PhoneDashboard/index.jsx
+++ src/components/PhoneDashboard/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PhoneList from '../PhoneList';
+import PhoneCart from '../PhoneCart';
 
 class PhoneDashboard extends React.Component {
   constructor(props) {
@@ -7,9 +8,30 @@ class PhoneDashboard extends React.Component {
 
     this.state = {
       isPhoneShown: true,
+      isDirectSortOrder: true,
+      phones: [
+        {
+          id: 0,
+          color: 'black',
+          price: 15999.99,
+          isFavorite: false,
+        },
+        {
+          id: 1,
+          color: 'white',
+          price: 9999.99,
+          isFavorite: false,
+        },
+        {
+          id: 2,
+          color: 'gold',
+          price: 59999.99,
+          isFavorite: false,
+        },
+      ],
+      cart: [],
     };
   }
-
 
   handleVisibility = () => {
     this.setState({
@@ -17,13 +39,91 @@ class PhoneDashboard extends React.Component {
     });
   };
 
+  toggleSortOrder = () => {
+    /*
+      1. взяти масив зі стейту
+      2. перевернути його
+      3. оновити стан, не забути змінити змінну яка стежить за порядком масиву
+    */
+
+    // 1. взяти масив зі стейту
+    const { phones, isDirectSortOrder } = this.state;
+
+    // 2. перевернути його
+    // const phonesCopy = JSON.parse(JSON.stringify(phones));
+    const phonesCopy = structuredClone(phones);
+    phonesCopy.reverse();
+
+    // 3. оновити стан, не забути змінити змінну яка стежить за порядком масиву
+    this.setState({
+      isDirectSortOrder: !isDirectSortOrder,
+      phones: phonesCopy,
+    });
+  };
+
+  toggleFavorite = (id) => {
+    const { phones } = this.state;
+    /*
+      1. знайти телефон
+      2. змінити дані у потрідному телефоні
+      3. оновити стан
+    */
+
+    // 1. знайти телефон
+    //  через id
+    // 2. змінити дані у потрідному телефоні
+    const newPhones = phones.map((phone) => {
+      if (id !== phone.id) {
+        return phone;
+      }
+
+      return {
+        ...phone,
+        isFavorite: !phone.isFavorite,
+      };
+    });
+
+    // 3. оновити стан
+    this.setState({
+      phones: newPhones,
+    });
+  };
+
+  addToCart = (id) => {
+    const { phones, cart } = this.state;
+    const phoneToAdd = phones.find((phone) => id === phone.id);
+
+    const cartIndex = cart.findIndex((phone) => id === phone.id);
+
+    let newCart;
+
+    if (cartIndex === -1) {
+      newCart = [...cart, { ...phoneToAdd, quantity: 1 }];
+    } else {
+      newCart = [...cart];
+      newCart[cartIndex].quantity++;
+    }
+
+    this.setState({
+      cart: newCart,
+    });
+  };
+
   render() {
-    const { isPhoneShown } = this.state;
+    const { isPhoneShown, phones, cart } = this.state;
 
     return (
       <>
         <button onClick={this.handleVisibility}>Toggle phone visibility</button>
-        {isPhoneShown && <PhoneList />}
+        {isPhoneShown && (
+          <PhoneList
+            phones={phones}
+            addToCart={this.addToCart}
+            toggleFavorite={this.toggleFavorite}
+            toggleSortOrder={this.toggleSortOrder}
+          />
+        )}
+        <PhoneCart cart={cart} />
       </>
     );
   }
--- src/components/PhoneList/index.jsx
+++ src/components/PhoneList/index.jsx
@@ -1,110 +1,16 @@
 import React from 'react';
 import Phone from '../Phone';
-import PhoneCart from '../PhoneCart';
 
 class PhoneList extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      isDirectSortOrder: true,
-      phones: [
-        {
-          id: 0,
-          color: 'black',
-          price: 15999.99,
-          isFavorite: false,
-        },
-        {
-          id: 1,
-          color: 'white',
-          price: 9999.99,
-          isFavorite: false,
-        },
-        {
-          id: 2,
-          color: 'gold',
-          price: 59999.99,
-          isFavorite: false,
-        },
-      ],
-      cart: [],
-    };
   }
 
-  toggleSortOrder = () => {
-    /*
-      1. взяти масив зі стейту
-      2. перевернути його
-      3. оновити стан, не забути змінити змінну яка стежить за порядком масиву
-    */
-
-    // 1. взяти масив зі стейту
-    const { phones, isDirectSortOrder } = this.state;
-
-    // 2. перевернути його
-    // const phonesCopy = JSON.parse(JSON.stringify(phones));
-    const phonesCopy = structuredClone(phones);
-    phonesCopy.reverse();
-
-    // 3. оновити стан, не забути змінити змінну яка стежить за порядком масиву
-    this.setState({
-      isDirectSortOrder: !isDirectSortOrder,
-      phones: phonesCopy,
-    });
-  };
-
-  toggleFavorite = (id) => {
-    const { phones } = this.state;
-    /*
-      1. знайти телефон
-      2. змінити дані у потрідному телефоні
-      3. оновити стан
-    */
-
-    // 1. знайти телефон
-    //  через id
-    // 2. змінити дані у потрідному телефоні
-    const newPhones = phones.map((phone) => {
-      if (id !== phone.id) {
-        return phone;
-      }
-
-      return {
-        ...phone,
-        isFavorite: !phone.isFavorite,
-      };
-    });
-
-    // 3. оновити стан
-    this.setState({
-      phones: newPhones,
-    });
-  };
-
-  addToCart = (id) => {
-    const { phones, cart } = this.state;
-    const phoneToAdd = phones.find((phone) => id === phone.id);
-    
-    const cartIndex = cart.findIndex((phone) => id === phone.id);
-
-    let newCart;
-
-    if(cartIndex === -1) {
-      newCart = [...cart, {...phoneToAdd, quantity: 1}];
-    } else {
-      newCart = [...cart];
-      newCart[cartIndex].quantity++;
-    }
-    
-
-    this.setState({
-      cart: newCart
-    });
-  };
+  
 
   render() {
-    const { phones, isDirectSortOrder, cart } = this.state;
+
+    const { phones, toggleFavorite, addToCart, isDirectSortOrder, toggleSortOrder } = this.props;
 
     // const elems = (
     //   <>
@@ -122,8 +28,8 @@ class PhoneList extends React.Component {
       <Phone
         key={phone.id}
         phone={phone}
-        toggleFavorite={this.toggleFavorite}
-        addToCart={this.addToCart}
+        toggleFavorite={toggleFavorite}
+        addToCart={addToCart}
       />
     ));
 
@@ -134,12 +40,11 @@ class PhoneList extends React.Component {
 
     return (
       <>
-        <button onClick={this.toggleSortOrder}>Reverse sort order</button>
+        <button onClick={toggleSortOrder}>Reverse sort order</button>
         <p>
           Current sort order is: {isDirectSortOrder ? 'direct' : 'reversed'}
         </p>
         <ul>{phonesComponents}</ul>
-        <PhoneCart cart={cart} />
       </>
     );
   }
