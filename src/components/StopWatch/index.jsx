84a5b70a11a214844045f5e6fea8ca5e2b1e7511
Kyrylo <fresh.js.ftd.@gmail.com>
2024-01-18 18:09:33 +0200

    Add timeoutStopWatch example

--- src/components/StopWatch/index.jsx
+++ src/components/StopWatch/index.jsx
@@ -1,50 +1,122 @@
 import React, { Component } from 'react';
 import { format, addSeconds, add } from 'date-fns';
 
+// class StopWatch extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       date: new Date(0, 0, 0, 0, 0, 0),
+//     };
+//   }
+
+//   start = () => {
+//     if (!this.intervalId) {
+//       this.intervalId = setInterval(this.tick, 1000);
+//     }
+//   };
+
+//   stop = () => {
+//     clearInterval(this.intervalId);
+//     this.intervalId = null;
+//   };
+
+//   tick = () => {
+//     const { date } = this.state;
+
+//     // const newDate = addSeconds(date, 1);
+
+//     const newDate = add(date, { seconds: 1 });
+
+//     this.setState({
+//       date: newDate,
+//     });
+//   };
+
+//   componentDidMount() {
+//     this.start();
+//   }
+
+//   componentWillUnmount() {
+//     this.stop();
+//   }
+
+//   render() {
+//     const { date } = this.state;
+
+//     const timeText = format(date, 'HH:mm:ss');
+
+//     return (
+//       <div>
+//         <p>{timeText}</p>
+//         <button onClick={this.start}>Start</button>
+//         <button onClick={this.stop}>Stop</button>
+//       </div>
+//     );
+//   }
+// }
+
 class StopWatch extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      date: new Date(0, 0, 0, 0, 0, 0),
+      time: new Date(0, 0, 0, 0, 0, 0),
+      isRunning: false,
     };
+    this.timeoutId = null;
   }
 
   start = () => {
-    if (!this.intervalId) {
-      this.intervalId = setInterval(this.tick, 1000);
-    }
+    this.setState({
+      isRunning: true,
+    });
   };
 
   stop = () => {
-    clearInterval(this.intervalId);
-    this.intervalId = null;
-  };
+    this.setState({
+      isRunning: false,
+    });
+  }
 
   tick = () => {
-    const { date } = this.state;
+    const { time } = this.state;
 
-    // const newDate = addSeconds(date, 1);
-
-    const newDate = add(date, { seconds: 1 });
-
+    const newTime = addSeconds(time, 1);
     this.setState({
-      date: newDate,
+      time: newTime,
     });
   };
+
+  clear = () => {
+    clearTimeout(this.timeoutId);
+    this.timeoutId = null;
+  }
 
   componentDidMount() {
     this.start();
   }
 
-  componentWillUnmount() {
-    this.stop();
+  componentDidUpdate() {
+    const { isRunning } = this.state;
+
+    this.clear();
+
+    if(isRunning) {
+      this.timeoutId = setTimeout(this.tick, 1000)
+    }
   }
+  
+  
+  componentWillUnmount() {
+    this.clear();
+  }
+  
 
   render() {
-    const { date } = this.state;
+    const { time } = this.state;
 
-    const timeText = format(date, 'HH:mm:ss');
+    const timeText = format(time, 'HH:mm:ss');
 
     return (
       <div>
