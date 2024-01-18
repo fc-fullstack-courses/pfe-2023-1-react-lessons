d5458ce6b0c0c03395905afdbbdc2edd67f2153a
Kyrylo <fresh.js.ftd.@gmail.com>
2024-01-18 18:58:39 +0200

    Add controlled input example

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '12345admin',
+    };
+  }
 
   handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log(e.target.elements.email.value);
-    console.log(e.target.elements.password.value);
+    // console.log(e.target.elements.email.value);
+    // console.log(e.target.elements.password.value);
     // const {} = e;
 
-    e.target.reset();
-  }
+    // e.target.reset();
+  };
+
+  handleEmailChange = (e) => {
+    console.log(e.target.value);
+
+    this.setState({
+      email: e.target.value
+    });
+  };
 
   render() {
+    const { email } = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
-        <input type="email" name="email" placeholder="Email" />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={this.handleEmailChange}
+        />
         <input type="password" name="password" placeholder="Password" />
         <button type="submit">Login</button>
       </form>
