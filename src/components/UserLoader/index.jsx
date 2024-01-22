538a56ebec92f09040564280a50063d6fb535fdb
Kyrylo <fresh.js.ftd.@gmail.com>
2024-01-22 20:14:42 +0200

    Add basic pagination example

--- src/components/UserLoader/index.jsx
+++ src/components/UserLoader/index.jsx
@@ -8,15 +8,17 @@ class UserLoader extends Component {
       users: [],
       isError: false,
       isLoading: false,
+      currentPage: 1,
     };
   }
 
   componentDidMount() {
+    const { currentPage } = this.state;
     this.setState({
       isLoading: true,
     });
 
-    fetch('https://randomuser.me/api/')
+    fetch(`https://randomuser.me/api/?results=20&seed=test&page=${currentPage}`)
       .then((res) => res.json())
       .then((data) => {
         // console.log(data);
@@ -37,6 +39,53 @@ class UserLoader extends Component {
       });
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.currentPage !== this.state.currentPage) {
+      const { currentPage } = this.state;
+      this.setState({
+        isLoading: true,
+      });
+
+      fetch(
+        `https://randomuser.me/api/?results=20&seed=test&page=${currentPage}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          // console.log(data);
+
+          this.setState({
+            users: data.results,
+          });
+        })
+        .catch((err) => {
+          this.setState({
+            isError: true,
+          });
+        })
+        .finally(() => {
+          this.setState({
+            isLoading: false,
+          });
+        });
+    }
+  }
+
+  handleNextPage = () => {
+    const { currentPage } = this.state;
+
+    this.setState({
+      currentPage: currentPage + 1,
+    });
+  };
+
+  handlePrevPage = () => {
+    const { currentPage } = this.state;
+
+    this.setState({
+      currentPage: currentPage > 1 ? currentPage - 1 : 1,
+    });
+  };
+
   render() {
     const { users, isError, isLoading } = this.state;
 
@@ -48,18 +97,30 @@ class UserLoader extends Component {
       return <h1>ERROR HAPPENED</h1>;
     }
 
-    const userCards = users.map(({ name: { first, last }, picture: {
-      thumbnail: src
-    }, login: {uuid} }) => (
-      <article key={uuid}>
-        <h2>
-          {first} {last}
-        </h2>
-        <img src={src} alt={`${first} ${last}`} />
-      </article>
-    ));
+    const userCards = users.map(
+      ({
+        name: { first, last },
+        picture: { thumbnail: src },
+        login: { uuid },
+      }) => (
+        <article key={uuid}>
+          <h2>
+            {first} {last}
+          </h2>
+          <img src={src} alt={`${first} ${last}`} />
+        </article>
+      )
+    );
 
-    return <div>{userCards}</div>;
+    return (
+      <div>
+        <div>
+          <button onClick={this.handlePrevPage}>Prev</button>
+          <button onClick={this.handleNextPage}>Next</button>
+        </div>
+        {userCards}
+      </div>
+    );
   }
 }
 
