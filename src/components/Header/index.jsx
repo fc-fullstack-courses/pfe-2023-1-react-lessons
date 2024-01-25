ab0c7e435e1d5cbbb9edd39493cc129ad418030a
Kyrylo <fresh.js.ftd.@gmail.com>
2024-01-25 20:36:24 +0200

    Add UserContext to Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,41 +1,49 @@
 import React from 'react';
 import NavList from '../NavList';
-import { ThemeContext } from '../../contexts';
+import { ThemeContext, UserContext } from '../../contexts';
 import styles from './Header.module.css';
 import CONSTANTS from '../../constants';
 const { THEMES } = CONSTANTS;
+
 class Header extends React.Component {
   render() {
-    const renderHeader = ([theme, setTheme]) => {
-      let currentTheme;
+    return (
+      <UserContext.Consumer>
+        {(user) => (
+          <ThemeContext.Consumer>
+            {([theme, setTheme]) => {
+              let currentTheme;
 
-      if (theme === THEMES.LIGHT) {
-        currentTheme = styles.lightTheme;
-      } else if (theme === THEMES.DARK) {
-        currentTheme = styles.darkTheme;
-      }
+              if (theme === THEMES.LIGHT) {
+                currentTheme = styles.lightTheme;
+              } else if (theme === THEMES.DARK) {
+                currentTheme = styles.darkTheme;
+              }
 
-      const classes = `${styles.header} ${currentTheme}`;
+              const classes = `${styles.header} ${currentTheme}`;
 
-      return (
-        <header className={classes}>
-          <h1>Our site</h1>
-          <NavList />
-          <p>Theme is {theme}</p>
-          <select
-            value={theme}
-            onChange={(e) => {
-              setTheme(e.target.value);
+              return (
+                <header className={classes}>
+                  <h1>Our site</h1>
+                  <h2>
+                    Hello {user.firstName} {user.lastName}
+                  </h2>
+                  <select
+                    value={theme}
+                    onChange={(e) => {
+                      setTheme(e.target.value);
+                    }}
+                  >
+                    <option value={THEMES.LIGHT}>Light theme</option>
+                    <option value={THEMES.DARK}>DarkTheme</option>
+                  </select>
+                </header>
+              );
             }}
-          >
-            <option value={THEMES.LIGHT}>Light theme</option>
-            <option value={THEMES.DARK}>DarkTheme</option>
-          </select>
-        </header>
-      );
-    };
-
-    return <ThemeContext.Consumer>{renderHeader}</ThemeContext.Consumer>;
+          </ThemeContext.Consumer>
+        )}
+      </UserContext.Consumer>
+    );
   }
 }
 
