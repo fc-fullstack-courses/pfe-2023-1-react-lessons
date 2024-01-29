022c0b9ceaf4e8089965e56c45611e416b9ceef4
Kyrylo <fresh.js.ftd.@gmail.com>
2024-01-29 19:25:36 +0200

    Add more usefull BOM properties

--- src/bom.js
+++ src/bom.js
@@ -7,3 +7,17 @@
 // window.close() - закриває вікно, яке було відкрито у скрипті
 // window.moveTo() - технічно має переносити вікно
 // window.resizeTo() - технічно має змінювати розміри вікна
+
+// location - дані про урлу
+// location.hostname - доменне ім'я в урлі
+// location.pathname - шлях в урлі
+// location.search - параметри урли
+// location.assign(url) - відкриває посилання у поточнвій вкладці
+// location.reload() - перезавантаження сторінки
+// location.replace(url) - заміняє у історії вкладки поточний запис на вказаний
+
+// localStorage.setItem('key1', 'value 1'); - покласти значення у localStorage. 
+// І ключ і значення обов'язково рядки
+// localStorage.getItem(key) – отримати значення за ключем.
+// localStorage.removeItem(key); - видалити запис за ключем
+// localStorage.clear(); - видалити всі записи зі стораджу для певної сторінки
