789ab37180c342ed0d40ed3153b56078dfab92d4
Kyrylo <fresh.js.ftd.@gmail.com>
2024-01-29 18:21:30 +0200

    Add some window methods and properties

---
+++ src/bom.js
@@ -0,0 +1,9 @@
+// BOM (Browser Object Model) - вхідна точка це об'єкт window
+// розміри в'юпорта браузера:
+// window.innerHeight 
+// window.innerWidth
+
+// window.open('https://youtube.com')
+// window.close() - закриває вікно, яке було відкрито у скрипті
+// window.moveTo() - технічно має переносити вікно
+// window.resizeTo() - технічно має змінювати розміри вікна
