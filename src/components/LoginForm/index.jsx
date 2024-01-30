34e93a63d2edddffe19a699020a723cd9a47a4b8
Kyrylo <fresh.js.ftd.@gmail.com>
2024-01-30 20:26:26 +0200

    Add basic Formik form example

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,96 +1,42 @@
-import React, { Component } from 'react';
 // import { object, number, string, date, mixed, array } from 'yup';
+import { Formik } from 'formik';
 import { loginSchema } from '../../utils/validationSchemas';
 import styles from './loginForm.module.css';
 
 const initialValues = {
   email: '',
   password: '',
-  isRemebered: false,
-  isEmailValid: true,
 };
 
-class LoginForm extends Component {
-  constructor(props) {
-    super(props);
+function LoginForm(props) {
 
-    this.state = {
-      ...initialValues,
-    };
+  function handleSubmit (values, actions) {
+    console.log(values);
+    console.log(actions);
+
+    actions.resetForm();
   }
 
-  handleSubmit = (e) => {
-    e.preventDefault();
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit} >
+      {(formikProps) => {
+        console.log(formikProps);
 
-    const { email, password } = this.state;
-
-    const validData = {
-      email: 'user@gmail.com',
-      password: '1234test2134',
-      code: 15,
-    };
-
-    const invalidData = {
-      email: 'user@gmail.com',
-      code: 15,
-    };
-
-    this.setState({
-      ...initialValues,
-    });
-  };
-
-  handleChange = ({ target: { name, value, checked, type } }) => {
-    const isCheckbox = type === 'checkbox';
-
-    const newState = {
-      [name]: isCheckbox ? checked : value,
-    };
-
-    this.setState(newState);
-  };
-
-  render() {
-    const { email, password, isRemebered, isEmailValid } = this.state;
-
-    const emailClassNames = `${styles.input} ${
-      isEmailValid ? styles.validInput : styles.invalidInput
-    }`;
-
-    return (
-      <form onSubmit={this.handleSubmit} className={styles.container}>
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          className={emailClassNames}
-          value={email}
-          onChange={this.handleChange}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          className={styles.input}
-          value={password}
-          onChange={this.handleChange}
-        />
-        <label>
-          <input
-            type="checkbox"
-            name="isRemebered"
-            checked={isRemebered}
-            onChange={this.handleChange}
-          />{' '}
-          Is remembered
-        </label>
-
-        <button className={styles.btn} type="submit">
-          Login
-        </button>
-      </form>
-    );
-  }
+        return (
+          <form onSubmit={formikProps.handleSubmit}>
+            <input
+              type="email"
+              name="email"
+              value={formikProps.values.email}
+              onChange={formikProps.handleChange}
+            />
+            <input />
+            <button>Submit</button>
+          </form>
+        );
+      }}
+    </Formik>
+  );
 }
 
 export default LoginForm;
