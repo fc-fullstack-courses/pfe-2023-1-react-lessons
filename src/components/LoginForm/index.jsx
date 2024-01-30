7c40b943d9dbea1cbd29e8aac9b46171c2a07d49
Kyrylo <fresh.js.ftd.@gmail.com>
2024-01-30 20:08:30 +0200

    Replace validation example

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -35,37 +35,6 @@ class LoginForm extends Component {
       code: 15,
     };
 
-    /*
-      validate / validateSync - повертає пройшовшу валідацію річ або кидає помилку
-      isValid / isValidSync - повертає булее значення чи пройшли дані валідацію
-
-      методи без суффікса Sync є аснхроними і повертають проміси з аналогічними даними
-    */
-
-    // асинхронні перевіркі
-    // loginSchema.isValid(validData).then(result => {console.log(result)});
-    // loginSchema.isValid(invalidData).then(result => {console.log(result)});
-
-    // loginSchema.validate(validData).then(result => {console.log(result)});
-    loginSchema
-      .validate(validData)
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((err) => console.dir(err));
-
-    // синхронні перевіркі
-    // const isValidLoginData = loginSchema.isValidSync(validData);
-    const validatedObj = loginSchema.validateSync(validData);
-
-    try {
-      loginSchema.validateSync(invalidData);
-    } catch (error) {
-      console.dir(error);
-    }
-
-    // console.log(res1);
-
     this.setState({
       ...initialValues,
     });
--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -5,3 +5,34 @@ export const loginSchema = yup.object({
   password: yup.string().required().min(8).max(16),
   code: yup.number(),
 });
+
+    /*
+      validate / validateSync - повертає пройшовшу валідацію річ або кидає помилку
+      isValid / isValidSync - повертає булее значення чи пройшли дані валідацію
+
+      методи без суффікса Sync є аснхроними і повертають проміси з аналогічними даними
+    */
+
+    // асинхронні перевіркі
+    // loginSchema.isValid(validData).then(result => {console.log(result)});
+    // loginSchema.isValid(invalidData).then(result => {console.log(result)});
+
+    // loginSchema.validate(validData).then(result => {console.log(result)});
+    // loginSchema
+    //   .validate(validData)
+    //   .then((result) => {
+    //     console.log(result);
+    //   })
+    //   .catch((err) => console.dir(err));
+
+    // синхронні перевіркі
+    // const isValidLoginData = loginSchema.isValidSync(validData);
+    // const validatedObj = loginSchema.validateSync(validData);
+
+    // try {
+    //   loginSchema.validateSync(invalidData);
+    // } catch (error) {
+    //   console.dir(error);
+    // }
+
+    // console.log(res1);
