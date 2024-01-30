5510a10d11f7b68e26fa187adb073d8538b11eff
Kyrylo <fresh.js.ftd.@gmail.com>
2024-01-30 20:38:10 +0200

    Add Form and Field component usage example

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,16 +1,16 @@
 // import { object, number, string, date, mixed, array } from 'yup';
-import { Formik } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import { loginSchema } from '../../utils/validationSchemas';
 import styles from './loginForm.module.css';
 
 const initialValues = {
   email: '',
   password: '',
+  // select: '',
 };
 
 function LoginForm(props) {
-
-  function handleSubmit (values, actions) {
+  function handleSubmit(values, actions) {
     console.log(values);
     console.log(actions);
 
@@ -18,23 +18,17 @@ function LoginForm(props) {
   }
 
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit} >
-      {(formikProps) => {
-        console.log(formikProps);
-
-        return (
-          <form onSubmit={formikProps.handleSubmit}>
-            <input
-              type="email"
-              name="email"
-              value={formikProps.values.email}
-              onChange={formikProps.handleChange}
-            />
-            <input />
-            <button>Submit</button>
-          </form>
-        );
-      }}
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Form id="test" className="test1234">
+        <Field type="email" name="email" />
+        <Field name="password" type="password" />
+        {/* <Field as="select" name="select">
+              <option value={1}>Value 1</option>
+              <option value={2}>Value 2</option>
+              <option value={3}>Value 3</option>
+            </Field> */}
+        <button>Submit</button>
+      </Form>
     </Formik>
   );
 }
