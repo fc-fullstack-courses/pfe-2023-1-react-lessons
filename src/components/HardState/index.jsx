b9c67e62541b8741f3bb7876484c31ee556fa503
Kyrylo <fresh.js.ftd.@gmail.com>
2024-02-05 20:42:06 +0200

    Add useReducer basic example

--- src/components/HardState/index.jsx
+++ src/components/HardState/index.jsx
@@ -1,28 +1,67 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useReducer } from 'react';
+
+const initialState = {
+  number: 0,
+  coords: {
+    x: 0,
+    y: 0,
+  },
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'number': {
+      const newState = {
+        ...state,
+        number: action.payload,
+      };
+
+      return newState;
+    }
+    default:
+      return state;
+  }
+}
 
 const HardState = (props) => {
+  // state - стан
+  // dispatch - функція відправки
+  // reducer - функція, яка оновлюе стан, приймає найсвіжіший state першим аргументом
+  // і action (набір додаткових даних для зміни стану) другим аргументом
+  // reducer має повертати новий стан на основі цих даних
+  // initialState - початкове значення стану
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { coords, number } = state;
+
   // їх тут 5+
-  const [coords, setCoords] = useState({ x: 0, y: 0 });
-  const [number, setNumber] = useState(1);
+  // const [coords, setCoords] = useState({ x: 0, y: 0 });
+  // const [number, setNumber] = useState(1);
 
   // тут теж 5+
   function handleChange({ target: { value } }) {
-    setNumber(+value);
+    const action = { type: 'number', payload: +value };
+
+    dispatch(action);
+
+    // setNumber(+value);
   }
 
-  function handleCoords ({clientX, clientY}) {
-    setCoords({
-      x: clientX,
-      y: clientY
-    });
+  function handleCoords({ clientX, clientY }) {
+    const action = {
+      type: 'coords',
+    };
+    // setCoords({
+    //   x: clientX,
+    //   y: clientY
+    // });
   }
 
-  useEffect(()=> {
+  useEffect(() => {
     document.body.addEventListener('mousemove', handleCoords);
     return () => {
       document.body.removeEventListener('mousemove', handleCoords);
-    }
-  },[]);
+    };
+  }, []);
 
   return (
     <div>
